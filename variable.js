8075f94994cf81a85c080c51fb677ffcdfa6e40c
EmoTab <mhmmd.imran02@gmail.com>
2022-08-01 21:43:37 +0600

    added recape of previous module

---
+++ variable.js
@@ -0,0 +1,33 @@
+// RECAPE OF PREVIOUS MODULE START
+
+//number type variable when the value is numeric
+var sunglassPrice = 120;
+
+//string type variable when the value is string
+var specialOneName = "Ariana Grande";
+
+//boolean type variable when the value is either true or false
+var isSingle = true;
+
+//two types of number: Integer: 120 and Float or Decimal: 3.45
+
+//variable naming convention: 1. Integer: 120, 2. Float or Decimal: 3.40.
+/**
+ * 1. Single Word
+ * 2. No gap or space
+ * 3. no quote
+ * 4. no reserved keyword
+ * 5. can not start with a number
+ * 6. can not contain dash
+ * 7. should use camelCase
+ */
+
+//Math operations: +, -, *, /
+//Shorthand: +=, -=, *=, /=, ++, --
+//Convert: parseInt, parseFloat
+//toFixed
+//Remainder: % [Modulus]
+
+//RECAPE OF PREVIOUS MODULE END
+
+//TODAYS MODULE START

937f80e9fb87dc49a1db9c0b5d39250d610a074c
EmoTab <mhmmd.imran02@gmail.com>
2022-08-02 14:55:58 +0600

    introduced with nested conditions

---
+++ nested-condition.js
@@ -0,0 +1,22 @@
+//NESTED CONDITIONS
+
+var math = true;
+var geometry = true;
+var straightLine = false;
+
+if (math === true){
+    if (geometry === true){
+        if (straightLine === true){
+
+        }
+        else {
+            console.log('do not use curved line');
+        }
+    }
+    else {
+        console.log('you are not pithagoras');
+    }
+}
+else {
+
+}

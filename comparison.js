b7a673f7057f76a897b246389284fa5641950ea0
EmoTab <mhmmd.imran02@gmail.com>
2022-08-02 13:08:08 +0600

    added practice of Compare Variable and Comparison Operator

---
+++ comparison.js
@@ -0,0 +1,56 @@
+//Always count, is left one is comparison than right one?
+// console.log(5 < 6); //Less than
+// console.log(5 > 6); //Greater than
+// console.log(5 == 6); //Equals
+// console.log(5 != 6); //Not equals
+
+// console.log(5 <= 6); //Less than or equals
+// console.log(5 >= 6); //Greater than or equals
+
+//We can also check comparison datas inside variable
+
+var num1 = 56;
+var num2 = 89;
+// console.log(num1 < num2); //Less than
+// console.log(num1 > num2); //Greater than
+// console.log(num1 === num2); //Equals (to find equals we can use 2 equal or 3 equal sign)
+// console.log(num1 !== num2); //Not Equals
+
+// console.log(num1 <= num2); //Less than or Equals
+// console.log(num1 >= num2); //Greater than or Equals
+
+
+
+// Multiple conditions 
+//To use multiple conditions we use && double amperson sign called 'and' || double pipe sign called 'or'
+//To full-fill all conditions amoung many conditions, we use and sign &&
+//To full-fill any one condition amoung many conditions, we use or sign ||
+//money1 > money2 && result1 > result2 && height1 > height2 //here we are saying, have to fullfill all conditions
+//money1 > money2 || result1 > result2 || height1 > height2 //here we are saying, have to full-fill any one condition
+
+/**
+ * Operator    Description
+ *   ==        equls to
+ *   ===       equal value and equal type
+ *   !=        not equals to
+ *   !==       not equal value or not equal type
+ *   >         greater than
+ *   <         less than
+ *   >=        greater than or equal to
+ *   <=        less than or equal to
+ */
+
+/** Comparison Operators
+ *  Code        Example        Result        var a = 5;
+ *  a < b       5 < 2          False         var b = 2;
+ *  a > b       5 > 2          True
+ *  a <= b      5 <= 2         False
+ *  a >= b      5 >= 2         True
+ *  a == b      5 == 2         False
+ *  a != b      5 != 2         True
+ */
+
+
+
+
+
